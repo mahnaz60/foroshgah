05566d7b526abb55796d661ceb2fc69c1d7920dd
mahnaz60 <mahnaz.mohebbi.70@gmail.com>
2024-06-10 00:47:16 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert("به سایت ما خوش آمدید");
+
+function myFunction() {
+    var txt;
+    if (confirm("به سبد خرید اضافه شد")) {
+      txt = "با موفقیت انجام شد";
+    } else {
+      txt = "انجام نشد";
+    }
+    // document.getElementById("demo").innerHTML = txt;
+  }
